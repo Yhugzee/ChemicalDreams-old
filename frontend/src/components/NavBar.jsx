3544e576f7ef82c172a7b51556107da4b5412d3b
Yhugzee <rodriguez.h@pm.me>
2023-02-01 15:49:15 +0100

    Add navbar and information about this website

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import "./NavBar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+
+const breakpoint = 992;
+
+export default function NavBar() {
+  const [showProfile] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [show, setShow] = useState(true);
+
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setWidth(window.innerWidth);
+
+      if (window.innerWidth > breakpoint) {
+        setToggleMenu(false);
+      }
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    let oldValue = 0;
+    let newValue = 0;
+
+    window.addEventListener("scroll", () => {
+      // Get the new Value
+      newValue = window.scrollY;
+
+      if (oldValue - newValue < 0) {
+        setShow(false);
+      } else if (oldValue - newValue > 0) {
+        setShow(true);
+      }
+
+      // Update the old value
+      oldValue = newValue;
+    });
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
+  return (
+    <nav className="nav-bar" style={show ? { top: 0 } : { top: -150 }}>
+      {toggleMenu === false ? (
+        <button type="button" onClick={toggleNavSmallScreen} className="btn">
+          <GiHamburgerMenu className="menuIcon" />
+        </button>
+      ) : (
+        <button type="button" onClick={toggleNavSmallScreen} className="btn">
+          <ImCross className="menuIcon" />
+        </button>
+      )}
+      <Link to="/">
+        <img
+          className="logo"
+          alt="ChemicalDreams Logo"
+          src="src/assets/chemicald-logo.svg"
+        />
+      </Link>
+      {(toggleMenu || width > breakpoint) && (
+        <ul className="liste">
+          <li className="items">Accueil</li>
+          <li className="items">Listes</li>
+          <li className="items">A propos</li>
+        </ul>
+      )}
+      <div className="button-right">
+        <div>
+          {showProfile && (
+            <ul className="listProfile">
+              <li className="itemsProfile">
+                <button type="button">
+                  <Link to="/profile" className="btn_listProfile">
+                    Profil
+                  </Link>
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+        <Link to="/login">
+          <CgProfile className="profile" />
+        </Link>
+      </div>
+    </nav>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,27 @@
-import Counter from "../components/Counter";
-import logo from "../assets/logo.svg";
+import React from "react";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <div>
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <main>
+        <div className="infos">
+          <p>
+            Bienvenue sur ChemicalDreams, ici, vous trouverez des informations
+            sur les drogues et les médicaments. Nous sommes là pour vous fournir
+            des informations fiables et à jour sur les différents types de
+            substances psychoactives et les médicaments utilisés pour traiter
+            les troubles liés à ces substances. Nous croyons en la
+            sensibilisation aux risques associés à l'utilisation de ces
+            substances et à leur impact sur la santé. Consultez notre site pour
+            en savoir plus sur les différentes drogues et médicaments
+            disponibles.
+          </p>
+        </div>
+      </main>
+    </div>
   );
 }
