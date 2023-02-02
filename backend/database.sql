4bdcae9d97dacff553de86ac2809327f19b39887
Yhugzee <rodriguez.h@pm.me>
2023-02-02 11:26:10 +0100

    create table user

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,12 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE user
+(
+	id       INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	username VARCHAR(100)        NOT NULL UNIQUE,
+	email    VARCHAR(100)        NOT NULL UNIQUE,
+	password VARCHAR(100)        NOT NULL,
+	role     VARCHAR(15)         NOT NULL DEFAULT 'utilisateur'
+) engine = InnoDB
+  default charset = latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+INSERT INTO user (username, email, password, role)
+VALUES ('Yhugzee', 'yhugzee@pm.me', 'bypassadmin', 'admin');
