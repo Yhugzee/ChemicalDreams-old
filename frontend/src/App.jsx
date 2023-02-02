1e60f2cf6c77b1eceb816fcac4e1569f2e322c91
Yhugzee <rodriguez.h@pm.me>
2023-02-02 11:34:38 +0100

    add route to liste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Login from "./pages/Login";
+import Liste from "./pages/Liste";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/liste" element={<Liste />} />
         </Routes>
       </Router>
     </div>
