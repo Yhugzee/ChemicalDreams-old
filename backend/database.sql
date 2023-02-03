df91d4e00da37d590980980b9e3c6306a6db9ae1
Yhugzee <rodriguez.h@pm.me>
2023-02-03 10:51:42 +0100

    create table drugs and article

--- backend/database.sql
+++ backend/database.sql
@@ -1,3 +1,11 @@
+CREATE TABLE drugs
+(
+	id      INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nom     VARCHAR(100)        NOT NULL,
+	contenu TEXT                NOT NULL
+) engine = InnoDB
+  default charset = latin1;
+
 CREATE TABLE user
 (
 	id       INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -10,3 +18,13 @@ CREATE TABLE user
 
 INSERT INTO user (username, email, password, role)
 VALUES ('Yhugzee', 'yhugzee@pm.me', 'bypassadmin', 'admin');
+
+CREATE TABLE article
+(
+	id      INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nom     VARCHAR(100)        NOT NULL,
+	contenu VARCHAR(100)        NOT NULL,
+	date    DATE                NOT NULL,
+	comment VARCHAR(100)        NOT NULL
+) engine = InnoDB
+  default charset = latin1;
